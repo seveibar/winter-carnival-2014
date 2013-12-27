fe5453cbf450d480a59904e5186b414ec1b04676
Seve <seveibar@gmail.com>
2013-12-27 02:12:30 -0500

    r1

---
+++ main (sev-e i's conflicted copy 2013-12-19).js
@@ -0,0 +1,47 @@
+window.onload = function(){
+
+  var canvas = $("#canvas")[0];
+  canvas.height = 120;
+  canvas.width = 900;
+  var context = canvas.getContext("2d");
+  var imgs = [];
+  var imgsLoaded = 0;
+  var totalImagesToLoad = 3;
+  for (var i = 0; i < totalImagesToLoad; i++){
+    imgs.push(new Image());
+    imgs[i].src = "imgs/img9x"+(i+1)+".png";
+    imgs[i].onload = function(){
+      imgsLoaded ++;
+      if (imgsLoaded == totalImagesToLoad){
+        allImagesLoaded();
+      }
+    };
+  }
+  function switchHeaderImage(img0,img1){
+    var at = 0;
+    var tt = 48;
+    var i1 = imgs[img0];
+    var i2 = imgs[img1];
+    var anim = setInterval(function(){
+      at++;
+      // var p = Math.pow(at/tt,5);
+      var p = 3/2 * -4. * (-0.5 * Math.pow(at/tt,2)+0.333333 * Math.pow(at/tt,3)); 
+      context.drawImage(i1,0,-p*120);
+      context.drawImage(i2,0,120-p*120);
+
+      if (at == tt){
+        clearInterval(anim);
+        return;
+      }
+    },1000/24);
+  }
+  function allImagesLoaded(){
+    var currentImage = 0;
+    switchHeaderImage(1,0);
+    setInterval(function(){
+      var nextImage = (currentImage+1) % imgs.length;
+      switchHeaderImage(currentImage,nextImage);
+      currentImage = nextImage;
+    },5000);
+  }
+};
--- main.js
+++ main.js
@@ -1,47 +1,70 @@
 window.onload = function(){
 
-  // var canvas = $("#canvas")[0];
-  // canvas.height = 120;
-  // canvas.width = 900;
-  // var context = canvas.getContext("2d");
-  // var imgs = [];
-  // var imgsLoaded = 0;
-  // var totalImagesToLoad = 3;
-  // for (var i = 0; i < totalImagesToLoad; i++){
-  //   imgs.push(new Image());
-  //   imgs[i].src = "imgs/img9x"+(i+1)+".png";
-  //   imgs[i].onload = function(){
-  //     imgsLoaded ++;
-  //     if (imgsLoaded == totalImagesToLoad){
-  //       allImagesLoaded();
-  //     }
-  //   };
-  // }
-  // function switchHeaderImage(img0,img1){
-  //   var at = 0;
-  //   var tt = 48;
-  //   var i1 = imgs[img0];
-  //   var i2 = imgs[img1];
-  //   var anim = setInterval(function(){
-  //     at++;
-  //     // var p = Math.pow(at/tt,5);
-  //     var p = 3/2 * -4. * (-0.5 * Math.pow(at/tt,2)+0.333333 * Math.pow(at/tt,3)); 
-  //     context.drawImage(i1,0,-p*120);
-  //     context.drawImage(i2,0,120-p*120);
+  // Carousel Code
+  $('.jcarousel').jcarousel({
+    wrap:"circular",
+    animation:1000,
+    transitions:Modernizr.csstransitions ? {
+        transforms:   Modernizr.csstransforms,
+        transforms3d: Modernizr.csstransforms3d,
+        easing:       'ease'
+    } : false
+  });
+  var carousel = $(".jcarousel").data("jcarousel");
 
-  //     if (at == tt){
-  //       clearInterval(anim);
-  //       return;
-  //     }
-  //   },1000/30);
-  // }
-  // function allImagesLoaded(){
-  //   var currentImage = 0;
-  //   switchHeaderImage(1,0);
-  //   setInterval(function(){
-  //     var nextImage = (currentImage+1) % imgs.length;
-  //     switchHeaderImage(currentImage,nextImage);
-  //     currentImage = nextImage;
-  //   },5000);
-  // }
+  var currentSlide = 0;
+  var numSlides = 4;
+  var timeToMove = 10;
+
+  function highlightCircle(){
+    for (var i = 0;i<numSlides;i++){
+      if (i == currentSlide){
+        $("#carousel-bubble-"+i).css({"opacity":1});
+      }else{
+        $("#carousel-bubble-"+i).css({"opacity":.75});
+      }
+    }
+  }
+
+  highlightCircle();
+
+  var titles = ["","Super cool text overlay!","Rick touches his residents","So Food! Much good!"]
+  $("#hero-text-overlay").fadeOut(0);
+  $(".carousel-bubble").click(function(){
+    var idn = parseInt($(this).attr("id").split("-")[2]);
+    var oldSlide = currentSlide;
+    currentSlide = idn;
+    timeToMove = 5;
+    if (currentSlide > oldSlide){
+      carousel.scroll("+=" + (currentSlide - oldSlide));
+    }
+    if (currentSlide < oldSlide){
+      carousel.scroll("-=" + (oldSlide - currentSlide));
+    }
+    $("#hero-text-overlay").fadeOut(400,function(){
+          $("#hero-text-overlay p").text(titles[currentSlide]);
+          if (titles[currentSlide] != ""){
+            $("#hero-text-overlay").fadeIn();
+          }
+        });
+    // $('.jcarousel').jcarousel('scroll', $("#slide" + idn));
+    highlightCircle();
+  });
+
+  setInterval(function(){
+      timeToMove -= .25;
+      if (timeToMove<=0){
+        currentSlide = (currentSlide + 1) % numSlides;
+        $("#hero-text-overlay").fadeOut(400,function(){
+          $("#hero-text-overlay p").text(titles[currentSlide]);
+          if (titles[currentSlide] != ""){
+            $("#hero-text-overlay").fadeIn();
+          }
+        });
+
+        carousel.scroll("+=1");
+        timeToMove = 5;
+        highlightCircle();
+      }
+  },250)
 };
